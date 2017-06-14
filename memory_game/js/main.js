e67167d2f8372a15f5d03f5f1c136f1382e7ec56
jessbakk <jessica.bakkers@gmail.com>
2017-06-14 12:05:47 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,23 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardFour);
+var cardsInPlay = [];
 
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again");
+	}
+};
 
 
