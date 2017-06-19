ae04ef96510000ace96cf123cd4b6f385b796146
jessbakk <jessica.bakkers@gmail.com>
2017-06-18 22:30:20 -0700

    created game logic unit 11 - added refresh

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,7 +23,7 @@ var cards = [
 
 ];
 var cardsInPlay = [];
-
+	
 var checkForMatch = function() {
 	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -34,17 +34,35 @@ var checkForMatch = function() {
 	}
 };
 
-var flipCard = function (cardId){
-	console.log("User flipped " + cards[cardId].rank);
+var flipCard = function (){
+	var cardId = this.getAttribute("data-id");
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
+	this.setAttribute("src", cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	};
+	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+};
+
+createBoard();
+
+function refresh() {
+	location.reload();
+}
+
 
 
 
